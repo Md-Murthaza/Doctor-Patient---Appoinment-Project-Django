205f92a89361d93973a138c0f32bf11db8157e86
Md-Murthaza <murthazamurtu@gmail.com>
2024-11-07 16:39:40 +0530

    added a base.html and doctorlist

--- doctorapp/views.py
+++ doctorapp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from .models import Doctor
 # Create your views here.
+
+def base_view(request):
+    return render(request,'Base.html')
+
+
+
+
+def doctor_list(request):
+    doctors = Doctor.objects.all()
+    return render(request,'doctor_list.html',{'doctors':doctors})
