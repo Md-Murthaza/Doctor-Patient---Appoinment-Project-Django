46353ea2421a11d0c479c4ff10d733ed4cf9a116
Md-Murthaza <murthazamurtu@gmail.com>
2024-11-08 11:43:32 +0530

    some changes

--- doctorapp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Patient,Doctor,Appoinments
-# Register your models here.
-
-admin.site.register(Patient)
-admin.site.register(Doctor)
-admin.site.register(Appoinments)
